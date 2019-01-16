7594e8b0601f324972689b37b8f510fbb2d6f9af
shahid1593 <46758409+shahid1593@users.noreply.github.com>
2019-01-16 23:45:55 +0530

    Add files via upload

---
+++ biometric.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 18, 2016 at 03:15 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `biometric`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `Name` varchar(10) NOT NULL,
+  `Adm_No` varchar(10) NOT NULL,
+  `Date` date NOT NULL,
+  `Dept` varchar(5) NOT NULL,
+  `Sem` varchar(1) NOT NULL,
+  `Sub1` varchar(10) NOT NULL,
+  `Sub2` varchar(10) NOT NULL,
+  `Sub3` varchar(10) NOT NULL,
+  `Hour1` int(1) NOT NULL,
+  `Hour2` int(1) NOT NULL,
+  `Hour3` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`Name`, `Adm_No`, `Date`, `Dept`, `Sem`, `Sub1`, `Sub2`, `Sub3`, `Hour1`, `Hour2`, `Hour3`) VALUES
+('Shahid S', 's12ec339', '2016-04-01', 'ECE', '8', 'CC', 'SAT', 'RTV', 1, 1, 0),
+('Nidhin S', 's12ec306', '2016-04-01', 'ECE', '8', 'CC', 'SAT', 'RTV', 1, 1, 1),
+('Sachin T', 's12ec239', '2016-04-01', 'ECE', '8', 'CC', 'SAT', 'RTV', 1, 1, 0),
+('Shabin S', 's12ec247', '2016-04-01', 'ECE', '8', 'CC', 'SAT', 'RTV', 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE IF NOT EXISTS `staff` (
+  `username` varchar(5) NOT NULL,
+  `password` varchar(5) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`username`, `password`) VALUES
+('rsk', 'rsk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `uname` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  PRIMARY KEY (`uname`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uname`, `password`) VALUES
+('s12ec239', 's12ec239'),
+('s12ec247', 's12ec247'),
+('s12ec306', 's12ec306'),
+('s12ec339', 's12ec339');
---
+++ localhost.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @author Geoffray Warnants
+ * @version 0.2b
+ */
+
+//
+// Database "biometric"
+//
+
+// biometric.attendance
+$attendance = array(
+  array('Name'=>'Shahid S','Adm_No'=>'s12ec339','Date'=>'2016-04-01','Dept'=>'ECE','Sem'=>8,'Sub1'=>'CC','Sub2'=>'SAT','Sub3'=>'RTV','Hour1'=>1,'Hour2'=>1,'Hour3'=>0),
+  array('Name'=>'Nidhin S','Adm_No'=>'s12ec306','Date'=>'2016-04-01','Dept'=>'ECE','Sem'=>8,'Sub1'=>'CC','Sub2'=>'SAT','Sub3'=>'RTV','Hour1'=>1,'Hour2'=>1,'Hour3'=>1),
+  array('Name'=>'Sachin T','Adm_No'=>'s12ec239','Date'=>'2016-04-01','Dept'=>'ECE','Sem'=>8,'Sub1'=>'CC','Sub2'=>'SAT','Sub3'=>'RTV','Hour1'=>1,'Hour2'=>1,'Hour3'=>0),
+  array('Name'=>'Shabin S','Adm_No'=>'s12ec247','Date'=>'2016-04-01','Dept'=>'ECE','Sem'=>8,'Sub1'=>'CC','Sub2'=>'SAT','Sub3'=>'RTV','Hour1'=>1,'Hour2'=>0,'Hour3'=>1)
+);
+
+// biometric.staff
+$staff = array(
+  array('username'=>'rsk','password'=>'rsk')
+);
+
+// biometric.user
+$user = array(
+  array('uname'=>'s12ec239','password'=>'s12ec239'),
+  array('uname'=>'s12ec247','password'=>'s12ec247'),
+  array('uname'=>'s12ec306','password'=>'s12ec306'),
+  array('uname'=>'s12ec339','password'=>'s12ec339')
+);
